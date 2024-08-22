348668fb1c9f174670b411b64101207dda4ed698
Portillo87 <wilfredovillata828@gmail.com>
2024-08-22 17:36:33 -0600

    PracticaUno

---
+++ Practicas/PracticaUno.kt
@@ -0,0 +1,35 @@
+fun main() {
+    println("Ingrese el primer número:")
+    val numeroUno = readLine()?.toDoubleOrNull()
+
+    println("Ingrese el segundo número:")
+    val numeroDos = readLine()?.toDoubleOrNull()
+
+    println("Ingrese la operación (+, -, *, /, ^):")
+    val operacion = readLine()
+
+    if (numeroUno == null || numeroDos == null || operacion.isNullOrBlank()) {
+        println("Error: Ingrese números válidos y una operación válida.")
+        return
+    }
+
+    val resultado = when (operacion) {
+        "+" -> numeroUno + numeroDos
+        "-" -> numeroUno - numeroDos
+        "*" -> numeroUno * numeroDos
+        "/" -> {
+            if (numeroDos == 0.0) {
+                println("Error: División por cero no permitida.")
+                return
+            }
+            numeroUno / numeroDos
+        }
+        "^" -> Math.pow(numeroUno, numeroDos)
+        else -> {
+            println("Operación no reconocida.")
+            return
+        }
+    }
+
+    println("El resultado de la operación $numeroUno $operacion $numeroDos es $resultado")
+}
